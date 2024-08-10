8247d6b3f075e0a72a6645c5193891f43eddd3cb
guillaumepgt <guillaume.prigent44@gmail.com>
2024-08-10 15:32:33 +0200

    users enter 2 number for the operation

--- app/src/main.rs
+++ app/src/main.rs
@@ -7,5 +7,10 @@ fn start() {
     println!("what operator do you want to enter + - * /");
     let mut input : String = String::new();
     std::io::stdin().read_line(&mut input).expect("Failed to read line");
-    println!("{}",input)
+    println!("enter the value in front of the operator");
+    let mut input_value_1 : String = String::new();
+    std::io::stdin().read_line(&mut input_value_1).expect("Failed to read line");
+    println!("enter the value in back of the operator");
+    let mut input_value_2 : String = String::new();
+    std::io::stdin().read_line(&mut input_value_2).expect("Failed to read line");
 }
